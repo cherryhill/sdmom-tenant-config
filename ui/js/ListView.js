a21cd694ff34175692fa5e9a8210d0451d1485a4
Peter Murray <jester@dltj.org>
2015-11-20 10:12:46 -0500

    Set relations pane view to 15 lines

--- ui/js/ListView.js
+++ ui/js/ListView.js
@@ -39,7 +39,7 @@ cspace = cspace || {};
             pagerModel: {
                 pageCount: 1,
                 pageIndex: 0,
-                pageSize: 50,
+                pageSize: 15,
                 sortDir: 1,
                 sortKey: "",
                 totalRange: 0
